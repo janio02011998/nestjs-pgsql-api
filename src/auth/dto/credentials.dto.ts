f4118e0a213ad9df472b54126dda49f2f568b5e9
Janio Carvalho <janiocarvalhojr@gmail.com>
2023-02-06 20:19:47 -0300

    fix: validator class

--- src/auth/dto/credentials.dto.ts
+++ src/auth/dto/credentials.dto.ts
@@ -1,4 +1,22 @@
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+
 export class CredentialsDto {
+  @IsNotEmpty({
+    message: 'Informe um endereço de email',
+  })
+  @IsEmail(
+    {},
+    {
+      message: 'Informe um endereço de email válido',
+    },
+  )
+  @MaxLength(200, {
+    message: 'O endereço de email deve ter menos de 200 caracteres',
+  })
   email: string;
+
+  @MinLength(6, {
+    message: 'A senha deve ter no mínimo 6 caracteres',
+  })
   password: string;
 }
