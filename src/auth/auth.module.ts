75a4579be6a501b452d7427314b6e8ef792c8ef6
Janio Carvalho <janiocarvalhojr@gmail.com>
2023-02-06 20:01:08 -0300

    chore(auth): improve module auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmExModule } from 'src/database/typeorm-ex.module';
+
 import { UserRepository } from 'src/users/users.repository';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     TypeOrmExModule.forCustomRepository([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: 'super-secret',
       signOptions: {
@@ -18,6 +21,7 @@ import { AuthService } from './auth.service';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
