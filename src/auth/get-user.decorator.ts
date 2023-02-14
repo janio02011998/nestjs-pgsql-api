d7a349fdcd100bacb1fe84714a08ddb74f562eb1
Janio Carvalho <janiocarvalhojr@gmail.com>
2023-02-13 21:19:17 -0300

    chore: add jwt validation on users controller

--- src/auth/get-user.decorator.ts
+++ src/auth/get-user.decorator.ts
@@ -2,5 +2,6 @@ import { createParamDecorator } from '@nestjs/common';
 import { User } from 'src/users/user.entity';
 
 export const GetUser = createParamDecorator((data, req): User => {
-  return req.user;
+  const user = req.args[0].user;
+  return user;
 });
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+
 import { UserRepository } from './users.repository';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+
 import { TypeOrmExModule } from 'src/database/typeorm-ex.module';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([UserRepository])],
+  imports: [
+    TypeOrmExModule.forCustomRepository([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   providers: [UsersService],
   controllers: [UsersController],
 })
