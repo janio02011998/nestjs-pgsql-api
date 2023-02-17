58822102ae64072a350c788cecb446cc5507bb60
Janio Carvalho <janiocarvalhojr@gmail.com>
2023-02-17 20:00:19 -0300

    fix: imports sort

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -1,6 +1,7 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
