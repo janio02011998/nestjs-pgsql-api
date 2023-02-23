e369d75126f66e06d2c397465c7b0fabddd7c0cd
Janio Carvalho <janiocarvalhojr@gmail.com>
2023-02-23 17:09:10 -0300

    chore: create dto to user find

--- .eslintrc.js
+++ .eslintrc.js
@@ -74,7 +74,7 @@ module.exports = {
         },
       },
     ],
-    'max-len': ["error", { "code": 80 }],
+    'max-len': ["error", { "code": 80, ignorePattern: '^import .*' }],
     '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
