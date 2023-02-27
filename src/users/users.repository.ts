c6cf7ef19e832b0eb219804547c05caeb66791ee
Janio Carvalho <janiocarvalhojr@gmail.com>
2023-02-27 18:59:43 -0300

    chore: create repository to find users

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -10,12 +10,45 @@ import { Repository } from 'typeorm';
 import { CustomRepository } from 'src/database/typeorm-ex.decorator';
 
 import { CreateUserDto } from './dto/create-user.dto';
+import { FindUsersQueryDto } from './dto/find-users-query.dto';
 import { UserRole } from './user-roles.enum';
 import { User } from './user.entity';
 import { CredentialsDto } from '../auth/dto/credentials.dto';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
+  async findUsers(
+    queryDto: FindUsersQueryDto,
+  ): Promise<{ users: User[]; total: number }> {
+    queryDto.status = queryDto.status === undefined ? true : queryDto.status;
+    queryDto.page = queryDto.page < 1 ? 1 : queryDto.page;
+    queryDto.limit = queryDto.limit > 100 ? 100 : queryDto.limit;
+
+    const { email, name, status, role } = queryDto;
+    const query = this.createQueryBuilder('user');
+    query.where('user.status = :status', { status });
+
+    if (email) {
+      query.andWhere('user.email ILIKE :email', { email: `%${email}%` });
+    }
+
+    if (name) {
+      query.andWhere('user.name ILIKE :name', { name: `%${name}%` });
+    }
+
+    if (role) {
+      query.andWhere('user.role = :role', { role });
+    }
+    query.skip((queryDto.page - 1) * queryDto.limit);
+    query.take(+queryDto.limit);
+    query.orderBy(queryDto.sort ? JSON.parse(queryDto.sort) : undefined);
+    query.select(['user.name', 'user.email', 'user.role', 'user.status']);
+
+    const [users, total] = await query.getManyAndCount();
+
+    return { users, total };
+  }
+
   async createUser(
     createUserDto: CreateUserDto,
     role: UserRole,
