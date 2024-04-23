e11661ed74878df50754063237ee40f2594ae90e
Janio Carvalho <janiocarvalhojr@gmail.com>
2024-04-23 18:12:46 -0300

    chore: added logs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { WinstonModule } from 'nest-winston';
 
 import { AuthModule } from './auth/auth.module';
 import { typeOrmConfig } from './configs/typeorm.config';
+import { winstonConfig } from './configs/winston.config';
+import { LoggerInterceptor } from './interceptors/logger.interceptor';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    WinstonModule.forRoot(winstonConfig),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
