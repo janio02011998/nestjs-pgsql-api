2500e9f5be450cc7fee517b8415a9e4acfb3ce20
Janio Carvalho <janiocarvalhojr@gmail.com>
2024-05-06 20:20:20 -0300

    feat: add test to user service

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { UserRepository } from './../users/users.repository';
+import { UsersService } from './users.service';
+
+const mockUserRepository = () => ({
+  createUser: jest.fn(),
+  findOne: jest.fn(),
+  delete: jest.fn(),
+  findUsers: jest.fn(),
+});
+
+describe('UsersService', () => {
+  let userRepository;
+  let service;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: UserRepository,
+          useFactory: mockUserRepository,
+        },
+      ],
+    }).compile();
+
+    userRepository = await module.get<UserRepository>(UserRepository);
+    service = await module.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+    expect(userRepository).toBeDefined();
+  });
+});
