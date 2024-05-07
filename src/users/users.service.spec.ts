a64a7662fa6ce1ecc4969e502deba7710e4fc834
Janio Carvalho <janiocarvalhojr@gmail.com>
2024-05-07 20:57:36 -0300

    feat: add tests to createAdminUsers

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,6 +1,9 @@
+import { UnprocessableEntityException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { UserRepository } from './../users/users.repository';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserRole } from './user-roles.enum';
 import { UsersService } from './users.service';
 
 const mockUserRepository = () => ({
@@ -32,5 +35,36 @@ describe('UsersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
     expect(userRepository).toBeDefined();
+  });
+
+  describe('createUser', () => {
+    let mockCreateUserDto: CreateUserDto;
+
+    beforeEach(() => {
+      mockCreateUserDto = {
+        email: 'mock@email.com',
+        name: 'Mock User',
+        password: 'mockPassword',
+        passwordConfirmation: 'mockPassword',
+      };
+    });
+
+    it('should create an user if passwords match', async () => {
+      userRepository.createUser.mockResolvedValue('mockUser');
+      const result = await service.createAdminUser(mockCreateUserDto);
+
+      expect(userRepository.createUser).toHaveBeenCalledWith(
+        mockCreateUserDto,
+        UserRole.ADMIN,
+      );
+      expect(result).toEqual('mockUser');
+    });
+
+    it('should throw an error if passwords doesnt match', async () => {
+      mockCreateUserDto.passwordConfirmation = 'wrongPassword';
+      expect(service.createAdminUser(mockCreateUserDto)).rejects.toThrow(
+        UnprocessableEntityException,
+      );
+    });
   });
 });
